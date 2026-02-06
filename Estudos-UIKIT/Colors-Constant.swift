2f1317706e5615b6ebc45ad45a11990728b56284
TecoAdamo <adamomateus478@gmail.com>
2026-02-06 03:01:38 -0300

    feat: Create screen with input and button

---
+++ Estudos-UIKIT/Colors-Constant.swift
@@ -0,0 +1,85 @@
+//
+//  Colors-Constant.swift
+//  Estudos-UIKIT
+//
+//  Created by Mateus on 06/02/26.
+//
+
+import Foundation
+import UIKit
+
+public struct Colors {
+
+    static let darkGrayBackground = UIColor(
+        red: 32/255,
+        green: 33/255,
+        blue: 36/255,
+        alpha: 1
+    )
+
+    static let darkGraySurface = UIColor(
+        red: 44/255,
+        green: 45/255,
+        blue: 49/255,
+        alpha: 1
+    )
+
+    static let grayBorder = UIColor(
+        red: 70/255,
+        green: 72/255,
+        blue: 78/255,
+        alpha: 1
+    )
+
+    static let grayTextPrimary = UIColor(
+        red: 235/255,
+        green: 236/255,
+        blue: 240/255,
+        alpha: 1
+    )
+
+    static let grayTextSecondary = UIColor(
+        red: 170/255,
+        green: 172/255,
+        blue: 178/255,
+        alpha: 1
+    )
+    
+//      //     //
+    
+    static let purplePrimary = UIColor(
+        red: 124/255,
+        green: 58/255,
+        blue: 237/255,
+        alpha: 1
+    )
+
+    static let purpleLight = UIColor(
+        red: 167/255,
+        green: 139/255,
+        blue: 250/255,
+        alpha: 1
+    )
+
+    static let purpleDark = UIColor(
+        red: 91/255,
+        green: 33/255,
+        blue: 182/255,
+        alpha: 1
+    )
+
+    // MARK: - Feedback
+    static let success = UIColor(
+        red: 34/255,
+        green: 197/255,
+        blue: 94/255,
+        alpha: 1
+    )
+
+    static let error = UIColor(
+        red: 239/255,
+        green: 68/255,
+        blue: 68/255,
+        alpha: 1
+    )
+}
--- Estudos-UIKIT/ViewController.swift
+++ Estudos-UIKIT/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let myButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("Clique aqui", for: .normal)
+        button.setTitleColor(Colors.darkGrayBackground, for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17)
+        button.backgroundColor = Colors.purpleDark
+        button.layer.cornerRadius = Metrics.small
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let myInput: UITextField = {
+        let input = UITextField()
+        input.attributedPlaceholder = NSAttributedString(
+            string: "Digite uma mensagem:",
+            attributes: [.foregroundColor: UIColor.white]
+        )
+        input.layer.borderWidth = 0.8
+        input.layer.borderColor = Colors.grayBorder.cgColor
+        input.layer.cornerRadius = Metrics.tiny
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: Metrics.tiny, height: 0))
+        input.leftView = paddingView
+        input.leftViewMode = .always
+        
+        input.translatesAutoresizingMaskIntoConstraints = false
+        return input
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = Colors.darkGrayBackground
+        
+        view.addSubview(myInput)
+        view.addSubview(myButton)
+        
+        NSLayoutConstraint.activate([
+            myInput.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myInput.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            myInput.widthAnchor.constraint(equalToConstant: 300),
+            myInput.heightAnchor.constraint(equalToConstant: 44),
+            
+            myButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            myButton.topAnchor.constraint(equalTo: myInput.bottomAnchor, constant: Metrics.huge),
+            myButton.widthAnchor.constraint(equalToConstant: 300),
+            myButton.heightAnchor.constraint(equalToConstant: Metrics.huge)
+        
+        ])
     }
-
-
 }
-
